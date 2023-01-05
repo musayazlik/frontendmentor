d7a749686fc1c6a39fcc7b204ff92426c0815a10
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-05 20:16:26 +0300

    Features field data.

---
+++ bookmark-landing-page/src/api/featuresData.js
@@ -0,0 +1,27 @@
+import FeaturesTab1 from '../assets/images/features-tab-1.svg'
+import FeaturesTab2 from '../assets/images/features-tab-2.svg'
+import FeaturesTab3 from '../assets/images/features-tab-3.svg'
+
+export const featuresData = [
+  {
+    tabName: 'Simple Bookmarking',
+    image: FeaturesTab1,
+    title: 'Bookmark in one click',
+    paragraph:
+      'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+  },
+  {
+    tabName: 'Speedy Searching',
+    image: FeaturesTab2,
+    title: 'Intelligent search',
+    paragraph:
+      'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+  },
+  {
+    tabName: 'Easy Sharing',
+    image: FeaturesTab3,
+    title: 'Share your bookmarks',
+    paragraph:
+      'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+  },
+]

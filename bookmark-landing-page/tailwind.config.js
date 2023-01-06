2cfe7175aedd09bf1a4cba104ece2b850e4d7a55
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-06 21:52:06 +0300

    Color variable arrangement

--- bookmark-landing-page/tailwind.config.js
+++ bookmark-landing-page/tailwind.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{html,js}'],
-  
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-600': '#5464db',
+        'zinc-700': '#242843',
+        'zinc-500': '#9694a6',
+      },
+    },
     container: {
       center: true,
       padding: '1rem',

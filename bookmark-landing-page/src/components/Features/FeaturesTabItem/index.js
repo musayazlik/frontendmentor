3bcf0dee3023fab58ec21a40f796d6376c936773
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-08 23:58:55 +0300

    Responsive edit.

--- bookmark-landing-page/src/components/Features/FeaturesTabItem/index.js
+++ bookmark-landing-page/src/components/Features/FeaturesTabItem/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 const index = ({ text, index, onClick, activeTab }) => {
   return (
-    <span
-      onClick={onClick}
-      className={` ${
-        activeTab === index
-          ? 'border-rose-600 text-zinc-700'
-          : ' border-transparent text-zinc-300'
-      } text-base px-12 py-4 border-b-4  font-normal cursor-pointer duration-300 `}>
-      {text}
-    </span>
+    <li key={index} onClick={onClick} className='border-b-2 w-full px-12 py-4 '>
+      <span
+        className={` ${
+          activeTab === index
+            ? 'border-rose-600 text-zinc-700'
+            : ' border-transparent text-zinc-300'
+        } text-base px-12 py-4 border-b-4 lg:border-b-4  font-normal cursor-pointer duration-300 `}>
+        {text}
+      </span>
+    </li>
   )
 }
 

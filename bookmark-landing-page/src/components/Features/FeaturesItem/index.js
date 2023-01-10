ed9e5d0c67c6f6c917c14a9d6f7c4f6af9e15516
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-10 13:39:40 +0300

    Responsive bugs fix.

--- bookmark-landing-page/src/components/Features/FeaturesItem/index.js
+++ bookmark-landing-page/src/components/Features/FeaturesItem/index.js
@@ -3,12 +3,12 @@ import Button from '../../Button'
 
 const FeaturesItem = ({ image, title, paragraph }) => {
   return (
-    <div className='grid grid-cols-12 justify-center  space-y-20 lg:space-y-0 lg:space-x-10 xl:space-x-28'>
+    <div className='grid grid-cols-12 justify-center  space-y-20 lg:space-y-0 lg:space-x-10 xl:space-x-28 '>
       <div className=' col-span-12 lg:col-span-6 flex justify-center '>
         <img src={image} alt='' className='w-full max-w-lg lg:max-w-2xl' />
       </div>
       <div className='w-full col-span-12 lg:col-span-6 flex flex-col justify-center items-center lg:items-start  '>
-        <h3 className='mb-6 text-2xl sm:text-4xl  font-semibold text-zinc-700 text-center lg:text-start'>
+        <h3 className='mb-6 text-2xl sm:text-4xl  font-semibold text-zinc-700 text-center lg:text-start mt-20 lg:mt-0'>
           {title}
         </h3>
         <p className='mb-10 text-md sm:text-xl text-zinc-400 font-normal max-w-xs sm:max-w-lg text-center lg:text-start'>

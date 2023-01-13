e9e109551f3b5245ce20fe85efd92748d8e89e18
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-13 23:48:24 +0300

    Responsive and bugs fix.

--- news-homepage-main/src/App.js
+++ news-homepage-main/src/App.js
@@ -8,11 +8,11 @@ function App() {
     <>
       <Header />
       <div className='container'>
-        <div className='grid grid-cols-4 gap-y-5 md:gap-y-0 md:gap-x-5'>
-          <div className='col-span-4 lg:col-span-2 xl:col-span-3'>
+        <div className='grid grid-cols-12 gap-y-7 lg:gap-y-0 md:gap-x-7'>
+          <div className='col-span-12 lg:col-span-7 xl:col-span-8'>
             <Content />
           </div>
-          <div className='col-span-4 lg:col-span-2 xl:col-span-1 bg-p-dark  px-6 pt-10'>
+          <div className='col-span-12 lg:col-span-5 xl:col-span-4 bg-p-dark  px-8 pt-10 mt-10 lg:mt-0'>
             <Sidebar />
           </div>
         </div>
--- news-homepage-main/src/components/sidebar/index.js
+++ news-homepage-main/src/components/sidebar/index.js
@@ -3,30 +3,30 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <>
-      <h2 className='font-bold text-4xl text-p-orange mb-6 '>New</h2>
+      <h2 className='font-bold text-6xl text-p-orange mb-10 '>New</h2>
       <div className='list py'>
-        <div className='listItem pb-8 border-b-2 border-p-light last:border-b-0 mb-8 last:mb-0 '>
-          <h3 className='text-white font-bold text-xl mb-2'>
+        <div className='listItem pb-10 border-b-2 border-p-light last:border-b-0 mb-10 last:mb-0 '>
+          <h3 className='text-white font-bold text-3xl lg:text-2xl xl:text-3xl mb-2'>
             Hydrogen VS Electric Cars
           </h3>
-          <p className='text-white/80'>
+          <p className='text-white/80 text-xl lg:text-lg xl:text-xl mt-4 leading-relaxed'>
             Will hydrogen-fueled cars ever catch up to EVs?
           </p>
         </div>
-        <div className='listItem pb-8 border-b-2 border-p-light last:border-b-0 mb-8 last:mb-0 '>
-          <h3 className='text-white font-bold text-xl mb-2'>
+        <div className='listItem pb-10 border-b-2 border-p-light last:border-b-0 mb-10 last:mb-0 '>
+          <h3 className='text-white font-bold text-3xl lg:text-2xl xl:text-3xl mb-2'>
             The Downsides of Al Artistry
           </h3>
-          <p className='text-white/80'>
+          <p className='text-white/80 text-xl lg:text-lg xl:text-xl mt-4 leading-relaxed'>
             What are the possible adverse effects of on-demand AI image
             generation?
           </p>
         </div>
-        <div className='listItem pb-8 border-b-2 border-p-light last:border-b-0 mb-8 last:mb-0 '>
-          <h3 className='text-white font-bold text-xl mb-2'>
+        <div className='listItem pb-10 border-b-2 border-p-light last:border-b-0 mb-8 last:mb-0 '>
+          <h3 className='text-white font-bold text-3xl lg:text-2xl xl:text-3xl mb-2'>
             Is VC Funding Drying Up?
           </h3>
-          <p className='text-white/80'>
+          <p className='text-white/80 text-xl lg:text-lg xl:text-xl mt-4 leading-relaxed'>
             Private funding by VC firms is down %50 YOY. We take a look at what
             that means.
           </p>
